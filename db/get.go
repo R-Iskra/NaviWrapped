58741b0ae555fbb0c5a2d940f98437046b8c4f89
R-Iskra <ryanmiskra@gmail.com>
2026-05-26 16:45:14 -0600

    added GetAlbum

--- db/get.go
+++ db/get.go
@@ -19,3 +19,19 @@ func GetArtist(name string) (int64, error) {
 
 	return id, nil
 }
+
+func GetAlbum(name string) (int64, error) {
+	stmt, err := DB.Prepare("SELECT id FROM albums WHERE name = ?")
+	if err != nil {
+		return 0, fmt.Errorf("prepare get album: %w", err)
+	}
+	defer stmt.Close()
+
+	var id int64
+	err = stmt.QueryRow(name).Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("get album: %w", err)
+	}
+
+	return id, nil
+}
