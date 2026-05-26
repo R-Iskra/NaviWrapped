b433e36b61bcf3aa21813dc9861dc674b6d3f068
R-Iskra <ryanmiskra@gmail.com>
2026-05-26 16:11:00 -0600

    added GetArtist

---
+++ db/get.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"fmt"
+)
+
+func GetArtist(name string) (int64, error) {
+	stmt, err := DB.Prepare("SELECT id FROM artists WHERE name = ?")
+	if err != nil {
+		return 0, fmt.Errorf("prepare get artist: %w", err)
+	}
+	defer stmt.Close()
+
+	var id int64
+	err = stmt.QueryRow(name).Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("get artist: %w", err)
+	}
+
+	return id, nil
+}
