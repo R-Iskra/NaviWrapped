8480aa5ab34db8a521733c229e7bf5534f55a872
R-Iskra <ryanmiskra@gmail.com>
2026-05-26 18:28:58 -0600

    add GetOrInsertAlbum

--- db/getorinsert.go
+++ db/getorinsert.go
@@ -7,3 +7,17 @@ func GetOrInsertArtist(name string) (int64, error) {
 	}
 	return InsertArtist(name)
 }
+
+func GetOrInsertAlbum(name string, artistName string) (int64, error) {
+	id, err := GetAlbum(name)
+	if err == nil {
+		return id, nil
+	}
+
+	artistID, err := GetOrInsertArtist(artistName)
+	if err != nil {
+		return 0, err
+	}
+
+	return InsertAlbum(name, artistID)
+}
